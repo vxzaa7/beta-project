d523c5fd1acf81b1718bc1d6d9ddf2fa4bbeedaa
vxzaa7 <vxzaa7@gmail.com>
2022-06-12 14:15:31 +0800

    做完表單

--- js/all.js
+++ js/all.js
@@ -27,4 +27,25 @@ const mobileNavbar = new MobileNavbar(
 	".mobile-nav",".main-nav__list",".main-nav__list li"
 )
 
+const mobileDarkNavbar = new MobileNavbar(
+	".mobile-nav",".dark-nav__list",".dark-nav__list li"
+)
+
 mobileNavbar.init();
+
+mobileDarkNavbar.init();
+
+
+function menuInput() {
+	let menu = document.getElementById('menu');
+	let menuTwo = document.getElementById('menuTwo');
+	let menuThree = document.getElementById('menuThree');
+
+
+	let txt = menu.options[menu.selectedIndex].text;
+
+	let txtTwo = menuTwo.options[menuTwo.selectedIndex].text;
+	let txtThree = menuThree.options[menuThree.selectedIndex].text;
+
+	document.getElementById("menu__input").value = txt +'|'+ txtTwo  +'|'+ txtThree;
+}
