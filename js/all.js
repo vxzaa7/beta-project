89c2f0b889fc84b1a392f3b8afa1d408d16fc533
vxzaa7 <vxzaa7@gmail.com>
2022-06-15 15:28:03 +0800

    做完project List的部分，即將做點擊後的輪播圖

--- js/all.js
+++ js/all.js
@@ -31,9 +31,18 @@ const mobileDarkNavbar = new MobileNavbar(
 	".mobile-nav",".dark-nav__list",".dark-nav__list li"
 )
 
+const mobileLightNavbar = new MobileNavbar(
+	".mobile-nav",".light-nav__list",".light-nav__list li"
+)
+
 mobileNavbar.init();
 
 mobileDarkNavbar.init();
+
+mobileLightNavbar.init();
+
+
+
 
 
 function menuInput() {
@@ -49,3 +58,55 @@ function menuInput() {
 
 	document.getElementById("menu__input").value = txt +'|'+ txtTwo  +'|'+ txtThree;
 }
+
+
+
+
+
+let list = document.querySelector('.project__list');
+function rander(data){
+	let str = '';
+	data.forEach((i)=>{
+			str+=`<div class="project__item" data-id="${i.id}">
+			<img src="./img/project/${i.imgName}"
+			alt="">
+
+					<div class="project__txt">
+						<p>${i.name}</p>
+						<a href="#" class="project__more">more <svg xmlns="http://www.w3.org/2000/svg"
+									 width="16"
+									 height="16"
+									 fill="currentColor"
+									 class="bi bi-chevron-right"
+									 viewBox="0 0 16 16">
+								<path fill-rule="evenodd"
+											d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+							</svg></a>
+					</div>
+				</div>
+			`;
+	});
+	list.innerHTML = str;
+}
+rander(data);
+
+
+
+
+let more = document.querySelectorAll(".project__more");
+let mask = document.querySelector(".layoutMask");
+let carousel = document.querySelector(".carousel-all");
+let close = document.querySelector(".close");
+
+
+more.forEach((i) => {
+	i.addEventListener('click', function () {
+		mask.classList.add('active');
+		carousel.classList.remove('active');
+	})
+});
+
+close.addEventListener('click', function () {
+	mask.classList.remove('active');
+		carousel.classList.add('active');
+})
---
+++ js/data.js
@@ -0,0 +1,49 @@
+const data =[
+	{
+		  id:001,
+			name:"台北護校基樁",
+			dete:"2019年2月28日",
+		imgName: "台北護校.jpeg",
+			txt:"大口徑基樁L=20米",
+			sort:"基樁"
+	},
+	{
+		id:002,
+		name:"地質改良工程",
+		dete: "2019年2月28日",
+		imgName: "蘭博地質改良.jpeg",
+		txt:"安全支撐及構台架設",
+		sort:"基樁"
+	},
+	{ id:003,
+		name:"達觀國小基樁施工",
+		dete:"2019年2月28日",
+		imgName: "達觀國小.jpeg",
+		txt:"大口徑120CM/18米基樁",
+		sort:"基樁"
+	},
+	{
+		id:004,
+		name:"三地門鋼軌樁",
+		dete: "2019年2月28日",
+		imgName: "三地門.jpeg",
+		txt:"大口徑基樁L=20米",
+		sort:"基樁"
+	},
+	{
+		id:005,
+		name:"大型機具出租",
+		dete: "2019年2月28日",
+		imgName: "001.jpeg",
+		txt:"機具出租",
+		sort:"出租"
+	},
+	{
+		id:006,
+		name:"台北護校6",
+		dete: "2019年2月28日",
+		imgName: "001.jpeg",
+		txt:"大口徑基樁L=20米",
+		sort:"基樁"
+	}
+]
